0076aaece0dff5ed4a3985615e3e209f2eb44348
Sooked <sookedofficial@gmail.com>
2022-04-08 17:55:35 +0530

    magic template

---
+++ evd.js
@@ -0,0 +1,13 @@
+function magic() {
+    document. getElementById("pre-magic").remove();
+    document.getElementById("post-magic").innerHTML = "Lol! Still you are waiting for magic. Do Your Work!";
+    setTimeout(timeoutstill, 2000)
+    setTimeout(timeoutload, 6000)
+}
+
+function timeoutstill(){
+    document.getElementById("time-magic").innerHTML = "Still you are waiting. Goo Now!";
+}
+function timeoutload(){
+    location.reload();
+}
